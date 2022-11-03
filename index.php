d7c6a661e38f7b91bb13eabf357d3ff13d17bb20
Lauren Fey <laurenfey@ucsb.edu>
2022-11-02 21:06:33 -0700

    first change

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
   <head>
-    <title>BreedID</title>
+    <title>Dog BreedID</title>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
--- my-handling-form-page.php
+++ my-handling-form-page.php
@@ -2,5 +2,5 @@
   // The global $_POST variable allows you to access the data sent with the POST method by name
   // To access the data sent with the GET method, you can use $_GET
   $user_name = htmlspecialchars($_POST['user_name']);
-  echo  $user_name;
+  echo Your name is $user_name;
 ?>
