1c138e46dc15ec3f0f532e895bb3c83afad4be33
Lauren Fey <laurenfey@ucsb.edu>
2022-11-07 20:55:05 -0800

    show uploaded img

--- file-upload.php
+++ file-upload.php
@@ -7,5 +7,14 @@
   $api = $cloudinary->adminApi();
 
   $uploaded = $uploader->upload($_FILES["file"]["tmp_name"]);
-  // echo $uploaded['public_id'];
+  // $uploaded = $uploader->upload('uploads/image.jpg');
+
+  $dom = new DOMDocument();
+  $dom->loadHTMLFile("index.php");
+
+  $img = $dom->getElementByID("dogphoto");
+  $img->setAttribute('src', "https://res.cloudinary.com/diee73kqp/image/upload/c_fill,g_face,h_299,w_299/" . $uploaded["public_id"]);
+  echo "https://res.cloudinary.com/diee73kqp/image/upload/c_fill,g_face,h_299,w_299/" . $uploaded["public_id"];
+
+  $content = $dom->saveHTMLFile("index.php");
 ?>
--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
+  <head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
     <title>Dog BreedID</title>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -10,42 +10,24 @@
   <body>
     <h1>BreedID</h1>
 
+    <!-- Import TensorFlow.js library -->
+    <script src="https://cdn.jsdelivr.net/npm/@tensorflow/tfjs@3.11.0/dist/tf.min.js" type="text/javascript"></script>
+    <script type="module" src="script.js" defer></script>
+
+    <!-- Import Dropzone -->
+    <script src="https://unpkg.com/dropzone@5/dist/min/dropzone.min.js"></script>
+    <link rel="stylesheet" href="https://unpkg.com/dropzone@5/dist/min/dropzone.min.css" type="text/css">
+
     <p id="status">Awaiting TF.js load</p>
 
-    <?php
-      require_once __DIR__ . '/vendor/autoload.php';
-      use Cloudinary\Cloudinary;
-      use Cloudinary\Tag\ImageTag;
-      use Cloudinary\Transformation\Resize;
-      use Cloudinary\Transformation\Gravity;
-      use Cloudinary\Transformation\FocusOn;
-      use Cloudinary\Transformation\RoundCorners;
-      use Cloudinary\Transformation\Delivery;
-      use Cloudinary\Transformation\Format;
+    <img src="https://res.cloudinary.com/diee73kqp/image/upload/c_fill,g_face,h_299,w_299/kxvwr96x3sv8cdrmsbgg" crossorigin="anonymous" id="dogphoto" tag="img">
 
-      $cloudinary = new Cloudinary();
-      $uploader = $cloudinary->uploadApi();
-      $api = $cloudinary->adminApi();
-
-      echo ImageTag::fromParams("fmuzmhqteyb7g2qzwonk", ["id"=>"dogphoto", "crossorigin" => "anonymous"])->resize(Resize::fill()->width(299)->height(299)->gravity(Gravity::focusOn(FocusOn::face())));
-    ?>
-
-    <script src="https://unpkg.com/dropzone@5/dist/min/dropzone.min.js"></script>
-    <link rel="stylesheet" href="https://unpkg.com/dropzone@5/dist/min/dropzone.min.css" type="text/css" />
-
-    <form action='file-upload.php' class="dropzone" id="upload" name="upload" method="post" enctype="multipart/form-data">
+    <form action="file-upload.php" class="dropzone" id="upload" name="upload" method="post" enctype="multipart/form-data">
       <p class="dz-message">Drop photo here to upload</p>
     </form>
 
     <button id="predict" type="button">Predict breed!</button>
 
     <p id="breed">We think this dog is a... </p>
-
-    <!-- Import TensorFlow.js library -->
-    <script src="https://cdn.jsdelivr.net/npm/@tensorflow/tfjs@3.11.0/dist/tf.min.js" type="text/javascript"></script>
-    <script type="module" src="script.js" defer></script>
-
-
-
   </body>
 </html>
