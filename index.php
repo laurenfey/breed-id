d5fa967fea82ee0b590325f5e68d0fbaf0add25a
Lauren Fey <laurenfey@ucsb.edu>
2022-11-08 19:05:35 -0800

    style changes

--- index.php
+++ index.php
@@ -1,14 +1,14 @@
 <!DOCTYPE html>
 <html lang="en">
   <head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-    <title>Dog BreedID</title>
+    <title>BreedReader</title>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="style.css">
   </head>
   <body>
-    <h1>BreedID</h1>
+    <h1>BreedReader</h1>
 
     <!-- Import TensorFlow.js library -->
     <script src="https://cdn.jsdelivr.net/npm/@tensorflow/tfjs@3.11.0/dist/tf.min.js" type="text/javascript"></script>
@@ -18,16 +18,16 @@
     <script src="https://unpkg.com/dropzone@5/dist/min/dropzone.min.js"></script>
     <link rel="stylesheet" href="https://unpkg.com/dropzone@5/dist/min/dropzone.min.css" type="text/css">
 
-    <p id="status">Awaiting TF.js load</p>
+    <p> BreedReader can guess your dog's breed from just a picture! </p>
 
     <form action="file-upload.php" class="dropzone" id="upload" name="upload" method="post" enctype="multipart/form-data">
-      <p class="dz-message">Drop photo here to upload</p>
-    </form>
+      <p class="dz-message">Upload a photo</p>
+    </form> <br>
 
-    <button id="predict" type="button">Predict breed!</button>
+    <button id="predict" type="button" style="display: block; margin: 0 auto;"> Predict Breed </button> <br>
 
-    <img crossorigin="anonymous" id="dogphoto" tag="img">
+    <img crossorigin="anonymous" id="dogphoto" tag="img"> <br>
+    <p id="breed"> </p>
 
-    <p id="breed">We think this dog is a... </p>
   </body>
 </html>
--- script.js
+++ script.js
@@ -25,9 +25,6 @@ function argMax(array) {
 const breed_json = '{"0" : "Akita","1" : "Alaskan Malamute","2" : "American Bully","3" : "American English Coonhound","4" : "American Eskimo Dog","5" : "American Foxhound","6" : "American Pit Bull Terrier","7" : "American Staffordshire Terrier","8" : "American Village Dog","9" : "Australian Cattle Dog","10" : "Australian Shepherd","11" : "Basset Hound","12" : "Beagle","13" : "Belgian Malinois","14" : "Bernese Mountain Dog","15" : "Bichon Frise","16" : "Black And Tan Coonhound","17" : "Bloodhound","18" : "Bluetick Coonhound","19" : "Border Collie","20" : "Boston Terrier","21" : "Boxer","22" : "Brittany","23" : "American Bulldog","24" : "English Bulldog","25" : "French Bulldog","26" : "Bullmastiff","27" : "Cane Corso","28" : "Catahoula Leopard Dog","29" : "Cavalier King Charles Spaniel","30" : "Chesapeake Bay Retriever","31" : "Chihuahua","32" : "Chinese Shar Pei","33" : "Chow Chow","34" : "Cocker Spaniel","35" : "Collie","36" : "Dachshund","37" : "Dalmatian","38" : "Doberman Pinscher","39" : "Elkhound","40" : "English Shepherd","41" : "English Springer Spaniel","42" : "German Shepherd","43" : "German Shorthaired Pointer","44" : "Golden Retriever","45" : "Gray Wolf","46" : "Great Dane","47" : "Great Pyrenees","48" : "Havanese","49" : "Kelpie","50" : "Labrador Retriever","51" : "Lhasa Apso","52" : "Llewellin Setter","53" : "Maltese","54" : "Mastiff","55" : "Miniature American Shepherd","56" : "Miniature Pinscher","57" : "Miniature Schnauzer","58" : "Mix","59" : "Mountain Cur","60" : "Neapolitan Mastiff","61" : "Pekingese","62" : "Pembroke Welsh Corgi","63" : "Perro De Presa Canario","64" : "Plott","65" : "Pointer","66" : "Pomeranian","67" : "Poodle","68" : "Miniature Poodle","69" : "Pug","70" : "Rat Terrier","71" : "Redbone Coonhound","72" : "Rottweiler","73" : "Russell Type Terrier","74" : "Saint Bernard","75" : "Samoyed","76" : "Shetland Sheepdog","77" : "Shih Tzu","78" : "Siberian Husky","79" : "Silky Terrier","80" : "Staffordshire Terrier","81" : "Toy Fox Terrier","82" : "Treeing Walker Coonhound","83" : "Weimaraner","84" : "Yorkshire Terrier"}';
 const breed_names = JSON.parse(breed_json);
 
-const status = document.getElementById("status");
-status.innerText = "Loaded TensorFlow.js - version: " + tf.version.tfjs;
-
 const breed_text = document.getElementById("breed");
 const dog_photo = document.getElementById("dogphoto");
 
@@ -40,6 +37,6 @@ async function predictBreed(image_id){
     const dog = tf.expandDims(tf.browser.fromPixels(dog_photo));
     const dog_scaled = tf.add(tf.div(dog,127.5),-1)
     const breed = await tf.argMax(tf.softmax(model.predict(dog_scaled)),1).array();
-    breed_text.textContent = "We think this dog is a... " + breed_names[breed[0]];
+    breed_text.textContent = breed_names[breed[0]];
   }
 }
